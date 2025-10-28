fe5b33cf8b3f556296d29a8592800717afee0c6c
LaraKorogix <larakorogix@gmail.com>
2025-10-27 22:39:36 -0300

    Correções de bugs - Atividade Programação Web

---
+++ vendas/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import Cliente, Produto, Venda, ItemVenda
+
+class ItemVendaInline(admin.TabularInline):
+    model = ItemVenda
+    extra = 1
+    fields = ['produto', 'quantidade', 'valor_unitario']
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email', 'data_cadastro']
+    search_fields = ['nome', 'email']
+    list_filter = ['data_cadastro']
+    ordering = ['nome']
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'valor', 'data_cadastro']
+    search_fields = ['nome']
+    list_filter = ['data_cadastro']
+    ordering = ['nome']
+
+@admin.register(Venda)
+class VendaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente', 'data_hora', 'valor_total']
+    list_filter = ['data_hora', 'cliente']
+    search_fields = ['cliente__nome']
+    inlines = [ItemVendaInline]
+    readonly_fields = ['valor_total']
+    
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        form.instance.calcular_total()
+
+@admin.register(ItemVenda)
+class ItemVendaAdmin(admin.ModelAdmin):
+    list_display = ['venda', 'produto', 'quantidade', 'valor_unitario', 'subtotal']
+    list_filter = ['venda__cliente', 'produto']
+    search_fields = ['venda__cliente__nome', 'produto__nome']
